7b8e376cbfadd37b77bca90f15ce200d223129f4
Yee Siang <exiang83@gmail.com>
2020-02-23 22:55:49 +0800

    fix ysUtil spelling

--- public_html/installer/index.php
+++ public_html/installer/index.php
@@ -4,10 +4,11 @@ $yii = dirname(__FILE__) . '/../../framework/yii.php';
 require_once $yii;
 require_once dirname(__FILE__) . '/../../protected/vendor/autoload.php';
 $config = array(
-    'defaultController' => 'default', 
-    'import'=>array(
-        'application.models.*',
-    )
+	'defaultController' => 'default',
+	'import' => array(
+		'application.vendor.*',
+		'application.models.*',
+	)
 );
 $app = Yii::createWebApplication($config);
 $app->run();
