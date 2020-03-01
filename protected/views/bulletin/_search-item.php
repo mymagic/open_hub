6d420926c7e260a4e6f23f550ca14b75a3e56f75
Yee Siang <exiang83@gmail.com>
2020-03-01 09:04:48 +0800

    remove views not in use

--- protected/views/bulletin/_search-item.php
+++
@@ -1,9 +0,0 @@
-<div class="bulletin-item">
-
-	<h3><a href="<?php echo $this->createUrl('bulletin/read', array('id' => $data->id)) ?>"><?php echo CHtml::encode($data->title); ?></a></h3>
-	
-	<p class="date"><?php echo Yii::app()->dateFormatter->formatDateTime($data->date_posted, 'long', null); ?></p>
-
-	<div class="text"><?php echo ysUtil::truncate(strip_tags($data->short_description), 250); ?></div>
-
-</div>
