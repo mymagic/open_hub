ec91c26467bcbe8eb5b33188d7d9dd0edcabdac2
Yee Siang <exiang83@yahoo.com>
2020-05-21 00:08:49 +0800

    hide suggestion if no result returned

--- protected/views/individualOrganization/backend/_view-organization-individual.php
+++ protected/views/individualOrganization/backend/_view-organization-individual.php
@@ -44,38 +44,40 @@
     <?php endif; ?>
 </div>
 
+<?php $result = HubIndividual::getRelatedEmailIndividual($model); ?>
+<?php if (!empty($result)): ?>
 <div class="row">
     <div class="col col-xs-12 margin-top-3x"> 
         <p><span class="label label-warning"><?php echo Yii::t('backend', 'Suggestion') ?></span> <?php echo Yii::t('backend', 'the following individuals may be related base on associated emails') ?></p>
     </div>
 </div>
 <div class="row">
-    <?php $result = HubIndividual::getRelatedEmailIndividual($model); ?>
-        <?php foreach ($result as $email => $individuals): ?>
-        
-        <div class="col-md-4 col-sm-6 margin-top-md">
-        <div class="border">
-            <div class="ibox-content text-center gray-bg">
-                <?php echo $email ?>
-            </div>
-            <?php foreach ($individuals as $individual): ?>
-            <div class="ibox-content text-center gray-bg">
-                <h3 class="text-muted"><?php echo $individual->full_name ?></h3>
-                <div class="m-b-sm">
-                    <?php echo Html::activeThumb($individual, 'image_photo', array('class' => 'img-circle')) ?>
-                </div>
-
-                <div class="text-center gray-bg">
-                    <a class="btn btn-xs btn-white" href="<?php echo $this->createUrl('/individual/view', array('id' => $individual->id)) ?>" target="_blank"><i class="fa fa-search"></i> View </a>
-                    <a class="btn btn-xs btn-primary" target="_blank" href="<?php echo $this->createUrl('/individualOrganization/create', array('individualId' => $individual->id, 'organizationCode' => $model->code)) ?>"><i class="fa fa-link"></i> Link</a>
-                </div>
-            </div>
-            <?php endforeach;?>
+    
+    <?php foreach ($result as $email => $individuals): ?>
+    
+    <div class="col-md-4 col-sm-6 margin-top-md">
+    <div class="border">
+        <div class="ibox-content text-center gray-bg">
+            <?php echo $email ?>
         </div>
-        </div>
+        <?php foreach ($individuals as $individual): ?>
+        <div class="ibox-content text-center gray-bg">
+            <h3 class="text-muted"><?php echo $individual->full_name ?></h3>
+            <div class="m-b-sm">
+                <?php echo Html::activeThumb($individual, 'image_photo', array('class' => 'img-circle')) ?>
+            </div>
 
-        <?php endforeach; ?>
+            <div class="text-center gray-bg">
+                <a class="btn btn-xs btn-white" href="<?php echo $this->createUrl('/individual/view', array('id' => $individual->id)) ?>" target="_blank"><i class="fa fa-search"></i> View </a>
+                <a class="btn btn-xs btn-primary" target="_blank" href="<?php echo $this->createUrl('/individualOrganization/create', array('individualId' => $individual->id, 'organizationCode' => $model->code)) ?>"><i class="fa fa-link"></i> Link</a>
+            </div>
+        </div>
+        <?php endforeach;?>
+    </div>
+    </div>
+
+    <?php endforeach; ?>
 </div>
-
+<?php endif; ?>
 
 
