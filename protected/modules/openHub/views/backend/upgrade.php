370f05c05f06332c4de1452fefa4fa87dbd18b6c
Yee Siang <exiang83@yahoo.com>
2020-05-28 13:50:49 +0800

    moved openhub upgrade js

--- protected/modules/openHub/views/backend/upgrade.php
+++ protected/modules/openHub/views/backend/upgrade.php
@@ -19,3 +19,24 @@
 </div>
 
 <?php Yii::app()->clientScript->registerScript('openHub-backend-upgrade', '') ?>
+
+<script>
+function streamUpgradeEvent() {
+    $('#btn-confirmUpgrade').addClass('disabled btn-default').removeClass('btn-primary');
+
+    var ta = document.getElementById('textarea-output');
+    //var source = new EventSource(ta.getAttribute('data-url'));
+    ta.value = ta.getAttribute('data-url');
+
+    source.addEventListener('message', function (e) {
+        if (e.data !== '') {
+            ta.value += e.data + '\n';
+        }
+    }, false);
+
+    source.addEventListener('error', function (e) {
+        source.close();
+    }, false);
+
+}
+</script>
