c9b6b8df747fbe801b1378d0f1f427a847d3bf42
Yee Siang <exiang83@yahoo.com>
2020-07-30 15:19:11 +0800

    fix upgrade

--- protected/modules/f7/upgrades/upgrade-1.4.php
+++ protected/modules/f7/upgrades/upgrade-1.4.php
@@ -1,6 +1,6 @@
 <?php
 
-function upgrade_module_1_3($about)
+function upgrade_module_1_4($about)
 {
 	$migration = Yii::app()->db->createCommand();
 	$migration->addColumn('form', 'json_extra', 'longtext NULL');
@@ -8,5 +8,5 @@ function upgrade_module_1_3($about)
 	$migration->addColumn('form_submission', 'process_by', 'varchar(128) NULL');
 	$migration->addColumn('form_submission', 'date_processed', 'integer(11) NULL');
 
-	return "Upgraded to version 1.3\n";
+	return "Upgraded to version 1.4\n";
 }
