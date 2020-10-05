5b9602ce3fa369d6773d3e8dc2ffbe7d480401a2
Yee Siang <exiang83@yahoo.com>
2020-10-05 21:16:40 +0800

    enable back renderSummary function

--- protected/components/widgets/GridView.php
+++ protected/components/widgets/GridView.php
@@ -199,45 +199,44 @@ class GridView extends CGridView
 		return $column;
 	}
 
-	/*public function renderSummary()
+	public function renderSummary()
 	{
-		if(($count=$this->dataProvider->getItemCount())<=0)
+		if (($count = $this->dataProvider->getItemCount()) <= 0) {
 			return;
-
-		echo '<div class="'.$this->summaryCssClass.'">';
-		if($this->enablePagination)
-		{
-			$pagination=$this->dataProvider->getPagination();
-			$total=$this->dataProvider->getTotalItemCount();
-			$start=$pagination->currentPage*$pagination->pageSize+1;
-			$end=$start+$count-1;
-			if($end>$total)
-			{
-				$end=$total;
-				$start=$end-$count+1;
-			}
-			if(($summaryText=$this->summaryText)===null)
-				$summaryText=Yii::t('zii','Displaying {start}-{end} of 1 result.|Displaying {start}-{end} of {count} results.',$total);
-			echo strtr($summaryText,array(
-				'{start}'=>$start,
-				'{end}'=>$end,
-				'{count}'=>$total,
-				'{page}'=>$pagination->currentPage+1,
-				'{pages}'=>$pagination->pageCount,
-			));
 		}
-		else
-		{
-			if(($summaryText=$this->summaryText)===null)
-				$summaryText=Yii::t('zii','Total 1 result.|Total {count} results.',$count);
-			echo strtr($summaryText,array(
-				'{count}'=>$count,
-				'{start}'=>1,
-				'{end}'=>$count,
-				'{page}'=>1,
-				'{pages}'=>1,
+
+		echo '<div class="' . $this->summaryCssClass . '">';
+		if ($this->enablePagination) {
+			$pagination = $this->dataProvider->getPagination();
+			$total = $this->dataProvider->getTotalItemCount();
+			$start = $pagination->currentPage * $pagination->pageSize + 1;
+			$end = $start + $count - 1;
+			if ($end > $total) {
+				$end = $total;
+				$start = $end - $count + 1;
+			}
+			if (($summaryText = $this->summaryText) === null) {
+				$summaryText = Yii::t('zii', 'Displaying {start}-{end} of 1 result.|Displaying {start}-{end} of {count} results.', $total);
+			}
+			echo strtr($summaryText, array(
+				'{start}' => $start,
+				'{end}' => $end,
+				'{count}' => $total,
+				'{page}' => $pagination->currentPage + 1,
+				'{pages}' => $pagination->pageCount,
+			));
+		} else {
+			if (($summaryText = $this->summaryText) === null) {
+				$summaryText = Yii::t('zii', 'Total 1 result.|Total {count} results.', $count);
+			}
+			echo strtr($summaryText, array(
+				'{count}' => $count,
+				'{start}' => 1,
+				'{end}' => $count,
+				'{page}' => 1,
+				'{pages}' => 1,
 			));
 		}
 		echo '</div>';
-	}*/
+	}
 }
