4d580d14e2755b05b92070c6a6e4667c881d55ba
Yee Siang <exiang83@yahoo.com>
2020-11-24 22:20:58 +0800

    added missing migration from magic_hub

---
+++ protected/migrations/m201116_070657_rename_column_merge_history.php
@@ -0,0 +1,28 @@
+<?php
+
+class m201116_070657_rename_column_merge_history extends CDbMigration
+{
+	public function up()
+	{
+		// admin_code to user_id
+		$this->renameColumn('individual_merge_history', 'admin_code', 'user_id');
+		$this->renameColumn('organization_merge_history', 'admin_code', 'user_id');
+	}
+
+	public function down()
+	{
+		echo "m201116_070657_rename_column_merge_history does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
